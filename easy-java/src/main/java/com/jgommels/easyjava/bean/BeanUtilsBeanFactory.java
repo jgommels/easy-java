a4a6ad3fe4e154b593212b1995ae9a7eeeee079e
Jared Gommels <jared@jgommels.com>
2017-09-16 09:36:38 -0500

    Added BigDecimalConverter and LocalDateTimeConverter.

--- easy-java/src/main/java/com/jgommels/easyjava/bean/BeanUtilsBeanFactory.java
+++ easy-java/src/main/java/com/jgommels/easyjava/bean/BeanUtilsBeanFactory.java
@@ -1,12 +1,16 @@
 package com.jgommels.easyjava.bean;
 
+import com.jgommels.easyjava.converter.BigDecimalConverter;
 import com.jgommels.easyjava.converter.InstantConverter;
 import com.jgommels.easyjava.converter.LocalDateConverter;
+import com.jgommels.easyjava.converter.LocalDateTimeConverter;
 import org.apache.commons.beanutils.BeanUtilsBean;
 import org.apache.commons.beanutils.ConvertUtilsBean;
 
+import java.math.BigDecimal;
 import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class BeanUtilsBeanFactory {
 
@@ -16,7 +20,9 @@ public class BeanUtilsBeanFactory {
     public static BeanUtilsBean getBeanUtilsBean() {
         ConvertUtilsBean convertUtilsBean = new ConvertUtilsBean();
         convertUtilsBean.register(new InstantConverter(), Instant.class);
-        convertUtilsBean.register(new LocalDateConverter(), LocalDate.class) ;
+        convertUtilsBean.register(new LocalDateConverter(), LocalDate.class);
+        convertUtilsBean.register(new LocalDateTimeConverter(), LocalDateTime.class);
+        convertUtilsBean.register(new BigDecimalConverter(), BigDecimal.class) ;
 
         return new BeanUtilsBean(convertUtilsBean);
     }
